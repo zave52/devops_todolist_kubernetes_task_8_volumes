49665a6c86c7caf8416239c2e7052773c9009854
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-10 10:30:02 +0300

    added bootstrap script for deploying all required resources in the cluster

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+echo "Applying PersistentVolume..."
+kubectl apply -f .infrastructure/pv.yml
+
+echo "Applying PersistentVolumeClaim..."
+kubectl apply -f .infrastructure/pvc.yml
+
+echo "Applying Secret..."
+kubectl apply -f .infrastructure/secret.yml
+
+echo "Applying ConfigMap..."
+kubectl apply -f .infrastructure/confgiMap.yml
+
+echo "Applying Deployment..."
+kubectl apply -f .infrastructure/deployment.yml
+
+echo "Deployment completed"
